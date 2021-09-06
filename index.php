6f53a923c425cfdfed623d379b9a025422a24f4c
0PW <smnelly@smnelly.ru>
2021-09-07 01:19:44 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html style="display:table;height:100%;width:100%;">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+
+<body style="display:table-cell;margin:0;vertical-align:middle;text-align:center;">
+
+<script type="text/javascript">
+
+</script>
+Жми!
+<a href="sj.php"><img src="SJ.jpg" width = "320" height = "240"  title = "Добро пожаловать!" align="top"></a>
+
+
+</body>
+</html>
+
+#0PW
